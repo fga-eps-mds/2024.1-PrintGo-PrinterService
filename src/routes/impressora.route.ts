c72c832e47190b65d617892789d7da1654da0819
Daniel Vinicius Alves <danielribeiroalves0@gmail.com>
2024-08-01 22:19:40 -0300

    fix[Test]: Corrige rotas que geram erros em testes.

--- src/routes/impressora.route.ts
+++ src/routes/impressora.route.ts
@@ -5,6 +5,7 @@ import { requestHandler } from '../middlewares/requestWrapper.adapter';
 const impressoraRoutes = Router();
 impressoraRoutes.post('/', requestHandler(ImpressoraController.createImpressora));
 impressoraRoutes.get('/', requestHandler(ImpressoraController.listImpressoras));
+impressoraRoutes.get('/:id', requestHandler(ImpressoraController.getImpressora));
 impressoraRoutes.get('/reports', requestHandler(ImpressoraController.listImpressorasReports));
 impressoraRoutes.patch('/:id', requestHandler(ImpressoraController.updateImpressora));
 impressoraRoutes.delete('/:id', requestHandler(ImpressoraController.deleteImpressora));
