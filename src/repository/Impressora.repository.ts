bc225dcc284945238530958777e28438bc15ce33
GRVial <guivial2010@gmail.com>
2024-08-03 20:10:52 -0300

    feature[repository]: adiciona funcao de atualizar contadores via snmp

--- src/repository/Impressora.repository.ts
+++ src/repository/Impressora.repository.ts
@@ -1,5 +1,7 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import { Impressora } from '../types/Impressora.type'
+import { getById } from "./Padrao.repository";
+import { getSnmpData} from "../snmp/snmpUtils"
 
 const impressoraClient = new PrismaClient().impressora;
 const prisma = new PrismaClient();
@@ -145,6 +147,56 @@ export const deleteImpressora = async (id: number): Promise<Impressora | false> 
         return impressora;
     } catch (error) {
         console.error("Erro ao desativar impressora:", error);
+        return false;
+    }
+};
+
+export const updatePrinterCounts = async (): Promise<boolean> => {
+    try {
+        const impressoras = await impressoraClient.findMany();
+
+        for (const impressora of impressoras) {
+            const modeloId = parseInt(impressora.modeloId, 10);
+            if (isNaN(modeloId)) {
+                console.error(`ID de modelo inválido: ${impressora.modeloId}`);
+                continue;
+            }
+
+            const padrao = await getById(modeloId);
+            if (!padrao) {
+                console.error(`Padrão não encontrado para o modelo: ${modeloId}`);
+                continue;
+            }
+
+            const oids = {
+                oidModelo: padrao.oidModelo,
+                oidNumeroSerie: padrao.oidNumeroSerie,
+                oidFirmware: padrao.oidFirmware,
+                oidTempoAtivo: padrao.oidTempoAtivo,
+                oidDigitalizacoes: padrao.oidDigitalizacoes,
+                oidCopiasPB: padrao.oidCopiasPB,
+                oidCopiasCor: padrao.oidCopiasCor,
+                oidTotalGeral: padrao.oidTotalGeral
+            };
+
+            const host = impressora.enderecoIp;
+            const oidsArray = Object.values(oids).filter(oid => oid !== null);
+            const snmpData = await getSnmpData(host, 161,oidsArray);    //era bom ter o port no banco caso n seja 161 padrao
+
+            const counts = {
+                contadorAtualPB: parseInt(snmpData[oids.oidCopiasPB] || '0', 10),
+                contadorAtualCor: parseInt(snmpData[oids.oidCopiasCor] || '0', 10)
+            };
+
+            await impressoraClient.update({
+                where: { id: impressora.id },
+                data: counts,
+            });
+        }
+
+        return true;
+    } catch (error) {
+        console.error('Erro ao atualizar contagens de impressão via SNMP:', error);
         return false;
     }
 };
