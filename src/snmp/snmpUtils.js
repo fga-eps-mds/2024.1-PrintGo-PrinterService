f8903be0ae66dada903e13a7baeccd35c4f2dec8
GRVial <guivial2010@gmail.com>
2024-08-04 11:09:52 -0300

    feature[snmp]: altera para getAll

--- src/snmp/snmpUtils.js
+++ src/snmp/snmpUtils.js
@@ -1,38 +1,23 @@
 const snmp = require('snmp-native');
 
-const oidStringToArray = (oidString) => {
-    return oidString.split('.').map(Number).filter(num => !isNaN(num));
-};
-
 const getSnmpData = (host, port, oids) => {
     return new Promise((resolve, reject) => {
         const session = new snmp.Session({ host: host, port: port, community: 'public' });
 
-        const oidsArray = oids.map(oidString => oidString.startsWith('.') ? oidString.slice(1) : oidString);
-        const oidsArrayFormatted = oidsArray.map(oidString => oidStringToArray(oidString));
+        const result = {};
 
-        const results = {};
-
-        let pendingRequests = oidsArrayFormatted.length;
-
-        oidsArrayFormatted.forEach(oid => {            
-            session.get({ oid: oid }, (error, varbinds) => {
-                if (error) {
-                    reject(error);
-                    session.close();
-                    return;
-                }
-
-                varbinds.forEach(vb => {
-                    results[vb.oid.join('.')] = vb.value.toString();
+        session.getAll({ oids: oids }, function (error, varbinds) {
+            if (error) {
+                reject(error);
+                session.close()
+                return;
+            } else {
+                varbinds.forEach(function (vb) {
+                    result[vb.oid] = vb.value;
                 });
-
-                pendingRequests -= 1;
-                if (pendingRequests === 0) {
-                    session.close();
-                    resolve(results);
-                }
-            });
+                resolve(result)
+            }
+            session.close();
         });
     });
 };
