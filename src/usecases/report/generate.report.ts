23501cbb43039fdc167d3ba6dffd0f40b08414d6
caiolamego <caio.lamego04@gmail.com>
2024-08-21 15:25:35 -0300

    hotfix[Tests]: corrigindo math.random.

--- src/usecases/report/generate.report.ts
+++ src/usecases/report/generate.report.ts
@@ -2,6 +2,7 @@ import { Impressora } from '../../types/Impressora.type';
 import { RelatorioData as ReportData } from '../../types/Relatorio.type';
 import { createReport } from './generate.pdf';
 import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 export const generateReport = (impressora: Impressora): ReportData => {
     const {
@@ -68,7 +69,7 @@ export const generateMonthReport = (impressora: Impressora): ReportData => {
 export const createPdf = async (reportData: ReportData): Promise<string | false> => {
     try {
         const pdfBuffer = await createReport(reportData);
-        const fileName: string = Math.random().toString(36).substring(7) + '.pdf';
+        const fileName: string = uuidv4() + '.pdf'; // Usando uuidv4 para gerar um identificador único
         const filePath: string = `./printer_reports/${fileName}`;
         // Escreve o buffer do PDF em um arquivo e cria diretório caso não exista
         fs.mkdirSync('./printer_reports', { recursive: true });
@@ -81,4 +82,5 @@ export const createPdf = async (reportData: ReportData): Promise<string | false>
         return false;
     }
 };
+
 
