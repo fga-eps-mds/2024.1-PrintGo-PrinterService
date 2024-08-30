6a32cb19e7fb9a5443145a71c4dd4907792166f8
MatheussBrant <matheus.s.brant@gmail.com>
2024-08-30 11:10:05 -0300

    feature[dashboard]: remove as requisições não utilizadas
    Refs: #47

--- src/controllers/Dashboard.controller.ts
+++ src/controllers/Dashboard.controller.ts
@@ -1,113 +1,11 @@
 import { request, Request, response, Response } from "express";
 import {
-    getTotalImpressions,
-    getEquipmentCountByLocation,
-    countColorPrinters,
-    countPbPrinters,
-    getSumOfCountersByImpressionType,
     getFiltroOpcoes,
-    getAllPrintersData,
     getDashboardData
 } from "../repository/dashboard.repository";
 import { getColorPrinterModelIds, getPbPrinterModelIds } from "../repository/Padrao.repository";
 
 export default {
-    async getTotalImpressions(request: Request, response: Response) {
-        const cidade = request.query.cidade as string;
-        const regional = request.query.regional as string;
-        const unidade = request.query.unidade as string;
-        const periodo = request.query.periodo as string; 
-
-        try {
-            const result = await getTotalImpressions({ cidade, regional, unidade, periodo });
-            return response.status(200).json(result);
-        } catch (error) {
-            return response.status(500).json({
-                error: true,
-                message: "Erro ao calcular impressões totais.",
-            });
-        }
-    },
-
-    async getColorPrintersCount(request: Request, response: Response) {
-        const cidade = request.query.cidade as string;
-        const regional = request.query.regional as string;
-        const unidade = request.query.unidade as string;
-        const periodo = request.query.periodo as string;
-    
-        try {
-            const colorModelIds = await getColorPrinterModelIds();
-            if (!colorModelIds.length) {
-                return response.status(200).json({ colorPrintersCount: 0 });
-            }
-            const colorPrintersCount = await countColorPrinters({ colorModelIds, cidade, regional, unidade, periodo });
-            return response.status(200).json({ colorPrintersCount });
-        } catch (error) {
-            return response.status(500).json({
-                error: true,
-                message: "Erro ao calcular o número de impressoras coloridas.",
-            });
-        }
-    },
-    
-
-    async getPbPrintersCount(request: Request, response: Response) {
-        const cidade = request.query.cidade as string;
-        const regional = request.query.regional as string;
-        const unidade = request.query.unidade as string;
-        const periodo = request.query.periodo as string;
-    
-        try {
-            const pbModelIds = await getPbPrinterModelIds();
-            if (!pbModelIds.length) {
-                return response.status(200).json({ PbPrintersCount: 0 });
-            }
-            const PbPrintersCount = await countPbPrinters({ pbModelIds, cidade, regional, unidade, periodo });
-            return response.status(200).json({ PbPrintersCount });
-        } catch (error) {
-            return response.status(500).json({
-                error: true,
-                message: "Erro ao calcular o número de impressoras preto e branco.",
-            });
-        }
-    },
-    
-
-    async getAllPrintersData(request: Request, response: Response) {
-        try {
-            const result = await getAllPrintersData();
-            return response.status(200).json({ data: result });
-        } catch (error) {
-            return response.status(500).json({
-                error: true,
-                message: "Erro ao calcular a soma dos contadores por localização.",
-            });
-        }
-    },
-
-    async getEquipmentCountByLocation(request: Request, response: Response) {
-        try {
-            const result = await getEquipmentCountByLocation();
-            return response.status(200).json({ data: result });
-        } catch (error) {
-            return response.status(500).json({
-                error: true,
-                message: "Erro ao contar o número de equipamentos por localização.",
-            });
-        }
-    },
-
-    async getSumOfCountersByImpressionType(request: Request, response: Response) {
-        try {
-            const result = await getSumOfCountersByImpressionType();
-            return response.status(200).json(result);
-        } catch (error) {
-            return response.status(500).json({
-                error: true,
-                message: "Erro ao calcular a soma total dos contadores.",
-            }); 
-        }
-    },
 
     async getFiltroOpcoes(request: Request, response: Response) {
         try {
--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -3,12 +3,6 @@ import { requestHandler } from '../middlewares/requestWrapper.adapter';
 import DashboardController from '../controllers/Dashboard.controller';
 
 const dashboardRoutes = Router();
-dashboardRoutes.get('/total-impressoes', requestHandler(DashboardController.getTotalImpressions));
-dashboardRoutes.get('/color-printers', requestHandler(DashboardController.getColorPrintersCount));
-dashboardRoutes.get('/pb-printers', requestHandler(DashboardController.getPbPrintersCount));
-dashboardRoutes.get('/all-printers-data', requestHandler(DashboardController.getAllPrintersData));
-dashboardRoutes.get('/equipment-by-location', requestHandler(DashboardController.getEquipmentCountByLocation));
-dashboardRoutes.get('/counter-by-type', requestHandler(DashboardController.getSumOfCountersByImpressionType));
 dashboardRoutes.get('/filtro-opcoes', requestHandler(DashboardController.getFiltroOpcoes));
 dashboardRoutes.get('/dashboard-data', requestHandler(DashboardController.getDashboardData));
 
