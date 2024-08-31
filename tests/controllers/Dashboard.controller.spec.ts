f6488efb987b9e1eb2d0840d5af23bedde401245
MatheussBrant <matheus.s.brant@gmail.com>
2024-08-31 00:32:58 -0300

    tests[dashboard]: implementa tests do getFIltros e getDashboardData
    Refs:#47

--- tests/controllers/Dashboard.controller.spec.ts
+++ tests/controllers/Dashboard.controller.spec.ts
@@ -1,24 +1,29 @@
 import request from 'supertest';
-import { server } from '../../src/server';
+import express from 'express';
 import { prisma } from '../../src/database';
+import dashboardRoutes from '../../src/routes/dashboard.route';
+import * as dashboardRepository from '../../src/repository/dashboard.repository'; // Importe o repositório
 
 describe("Dashboard Controller", () => {
-    afterAll(() => {
-        server.close();
-    });
+    const app = express();
+    app.use(express.json());
+    app.use('/dashboard', dashboardRoutes); // Adiciona manualmente as rotas
 
     afterEach(async () => {
+        await prisma.relatorio.deleteMany({});
         await prisma.impressora.deleteMany({});
         await prisma.padrao.deleteMany({});
     });
+
+    const uniqueNumSerie = () => `NUMSERIE_${Math.random().toString(36).substring(2, 15)}`;
 
     const defaultPrinter = {
         modeloId: "modeloXYZ",
         enderecoIp: "192.168.1.100",
         numContrato: "12345",
-        numSerie: "12345",
+        numSerie: uniqueNumSerie(),
         estaNaRede: true,
-        localizacao: "Anápolis;Workstation A;SubWorkstation 1",
+        localizacao: "Goiânia;Workstation A;Workstation B",
         dataInstalacao: "2024-01-01T00:00:00.000Z",
         contadorInstalacaoPB: 0,
         contadorInstalacaoCor: 0,
@@ -28,7 +33,7 @@ describe("Dashboard Controller", () => {
         contadorAtualCor: 50,
         ativo: true,
     };
-
+    
     const defaultPadrao = {
         modelo: "Imprime Baixo 3000",
         marca: "Epston",
@@ -43,126 +48,104 @@ describe("Dashboard Controller", () => {
         oidTotalGeral: "1.3.6.1.2.1.43.10.2.1.4.1.4"
     }
 
-    describe("GET /total-impressoes", () => {
-        it("should return the total number of copies", async () => {
-            const printer1 = {...defaultPrinter, contadorAtualPB: 100, contadorAtualCor: 50};
-            const printer2 = {...defaultPrinter, numSerie: "54321", contadorAtualPB: 200, contadorAtualCor: 150};
+    describe("GET /dashboard/filtro-opcoes", () => {
+        it("should return the filter options", async () => {
+            const printer1 = { ...defaultPrinter, numSerie: uniqueNumSerie(), dataContador: new Date('2024-08-01') };
+            const printer2 = { ...defaultPrinter, numSerie: uniqueNumSerie(), dataContador: new Date('2024-07-01') };
 
             await prisma.impressora.createMany({
                 data: [printer1, printer2],
             });
 
-            const res = await request(server).get('/dashboard/total-impressoes');
+            const res = await request(app).get('/dashboard/filtro-opcoes');
 
             expect(res.status).toBe(200);
-            expect(res.body.totalImpressions).toBe(500); // 100+50+200+150
+            expect(res.body).toEqual({
+                cidades: ['Goiânia'],
+                regionais: ['Workstation A'],
+                unidades: ['Workstation B'],
+                periodos: ['2024-08', '2024-07'],
+            });
         });
 
-        it("should return 500 if there is an error calculating total copies", async () => {
-            jest.spyOn(prisma.impressora, 'aggregate').mockImplementationOnce(() => {
+        it("should return 500 if there is an error fetching filter options", async () => {
+            // Mockando a função getFiltroOpcoes diretamente
+            const mockGetFiltroOpcoes = jest.spyOn(dashboardRepository, 'getFiltroOpcoes').mockImplementationOnce(() => {
                 throw new Error("Database Error");
             });
 
-            const res = await request(server).get('/dashboard/total-impressoes');
+            const res = await request(app).get('/dashboard/filtro-opcoes');
+
+            // Verificar se o mock foi chamado
+            expect(mockGetFiltroOpcoes).toHaveBeenCalled();
 
             expect(res.status).toBe(500);
-            expect(res.body.message).toBe("Erro ao calcular impressões totais.");
+            expect(res.body.message).toBe("Erro ao buscar opções de filtro.");
         });
     });
+    describe("GET /dashboard/dashboard-data", () => {
+        it("should return the dashboard data", async () => {
+            const padraoColor = { ...defaultPadrao, modelo: "colorModel", colorido: true };
+            const padraoPB = { ...defaultPadrao, modelo: "pbModel", colorido: false };
 
-    describe("GET /color-printers", () => {
-        it("should return the number of color printers", async () => {
-            const padraoColor = {...defaultPadrao, colorido: true};
-            const padrao = await prisma.padrao.create({
-                data: padraoColor,
-            });
-            const printer = {...defaultPrinter, modeloId: padrao.id.toString()};
-
-            await prisma.impressora.create({
-                data: printer,
+            await prisma.padrao.createMany({
+                data: [padraoColor, padraoPB],
             });
 
-            const res = await request(server).get('/dashboard/color-printers');
+            const printer1 = { ...defaultPrinter, numSerie: uniqueNumSerie(), modeloId: "colorModel" };
+            const printer2 = { ...defaultPrinter, numSerie: uniqueNumSerie(), modeloId: "pbModel" };
+            const printer3 = { ...defaultPrinter, numSerie: uniqueNumSerie(), modeloId: "pbModel" };
 
-            expect(res.status).toBe(200);
-            expect(res.body.colorPrintersCount).toBe(1);
-        });
-
-        it("should return 500 if there is an error calculating color printers count", async () => {
-            jest.spyOn(prisma.impressora, 'count').mockImplementationOnce(() => {
-                throw new Error("Database Error");
-            });
-
-            const res = await request(server).get('/dashboard/color-printers');
-
-            expect(res.status).toBe(500);
-            expect(res.body.message).toBe("Erro ao calcular o número de impressoras coloridas.");
-        });
-    });
-
-    describe("GET /pb-printers", () => {
-        it("should return the number of black and white printers", async () => {
-            const padraoPB = {...defaultPadrao, colorido: false};
-            const padrao = await prisma.padrao.create({
-                data: padraoPB,
-            });
-            const printer = {...defaultPrinter, modeloId: padrao.id.toString()};
-
-            await prisma.impressora.create({
-                data: printer,
-            });
-
-            const res = await request(server).get('/dashboard/pb-printers');
-
-            expect(res.status).toBe(200);
-            expect(res.body.PbPrintersCount).toBe(1);
-        });
-
-        it("should return 500 if there is an error calculating black and white printers count", async () => {
-            jest.spyOn(prisma.impressora, 'count').mockImplementationOnce(() => {
-                throw new Error("Database Error");
-            });
-
-            const res = await request(server).get('/dashboard/pb-printers');
-
-            expect(res.status).toBe(500);
-            expect(res.body.message).toBe("Erro ao calcular o número de impressoras pretas e brancas.");
-        });
-    });
-
-    describe("GET /impressions-by-location", () => {
-        it("should return the sum of counters by location", async () => {
-            const printer1 = {...defaultPrinter, contadorAtualPB: 100, contadorAtualCor: 50};
-            const printer2 = {...defaultPrinter, numSerie: "54321", contadorAtualPB: 200, contadorAtualCor: 150};
             await prisma.impressora.createMany({
-                data: [printer1, printer2],
+                data: [printer1, printer2, printer3],
             });
 
-            const res = await request(server).get('/dashboard/impressions-by-location');
+            const res = await request(app).get('/dashboard/dashboard-data');
 
             expect(res.status).toBe(200);
-            expect(res.body.data).toEqual([
-                { localizacao: "Anápolis;Workstation A;SubWorkstation 1", totalPB: 300, totalCor: 200 }
-            ]);
+            expect(res.body).toEqual({
+                impressoras: expect.arrayContaining([
+                    expect.objectContaining({
+                        localizacao: printer1.localizacao,
+                        dataContador: null,
+                        contadorAtualPB: 100,
+                        contadorAtualCor: 50,
+                        modeloId: "colorModel"
+                    }),
+                    expect.objectContaining({
+                        localizacao: printer2.localizacao,
+                        dataContador: null,
+                        contadorAtualPB: 100,
+                        contadorAtualCor: 50,
+                        modeloId: "pbModel"
+                    }),
+                    expect.objectContaining({
+                        localizacao: printer3.localizacao,
+                        dataContador: null,
+                        contadorAtualPB: 100,
+                        contadorAtualCor: 50,
+                        modeloId: "pbModel"
+                    })
+                ]),
+                totalColorPrinters: 1,
+                totalPbPrinters: 2,
+                colorModelIds: ["colorModel"],
+                pbModelIds: ["pbModel"]
+            });
         });
-    });
 
-    describe("GET /equipment-by-location", () => {
-        it("should return the equipment count by location", async () => {
-            const printer1 = defaultPrinter;
-            const printer2 = {...defaultPrinter, numSerie: "54321"};
-            await prisma.impressora.createMany({
-                data: [printer1, printer2],
+        it("should return 500 if there is an error fetching dashboard data", async () => {
+            const mockGetDashboardData = jest.spyOn(dashboardRepository, 'getDashboardData').mockImplementationOnce(() => {
+                throw new Error("Database Error");
             });
 
-            const res = await request(server).get('/dashboard/equipment-by-location');
+            const res = await request(app).get('/dashboard/dashboard-data');
 
-            console.log(res.body)
-
-            expect(res.status).toBe(200);
-            expect(res.body.data).toEqual([
-                {localizacao: "Anápolis;Workstation A;SubWorkstation 1", totalEquipamentos: 2}
-            ]);
+            expect(mockGetDashboardData).toHaveBeenCalled();
+            expect(res.status).toBe(500);
+            expect(res.body.message).toBe("Erro ao buscar os dados do dashboard.");
         });
     });
 });
+
+
